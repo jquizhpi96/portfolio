6f772c6666330aa697a57f576f298306a978f798
Jason Quizhpi <jasonquizhpi@Jasons-MacBook-Pro-2.local>
2021-09-21 12:45:10 -0400

    resume

--- website/src/Components/About/About.jsx
+++ website/src/Components/About/About.jsx
@@ -26,7 +26,7 @@ function About(props) {
 </a></p>
           <p>
             To learn more about my professional background
-            <a target="_blank" rel="noreferrer"href="https://drive.google.com/file/d/1w_4IdjqQfopMoPheTMAuIe6GcbH8RmOV/view?usp=sharing"> click here to view my
+            <a target="_blank" rel="noreferrer"href="https://drive.google.com/file/d/1lAgHniWbHrKhC9P8W4ZQqmwjoXcaZ6mj/view?usp=sharing"> click here to view my
             resume.
 </a>
           </p>
